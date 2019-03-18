67c568f0a21436d9804a8f025315b264282faf4d
isapas <isabelle.passeniers@gmail.com>
2019-03-18 15:26:57 +0100

    shell of application

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to testAngular!');
+    expect(page.getTitleText()).toEqual('Welcome to TourOfHeroes!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'testAngular'`, () => {
+  it(`should have as title 'TourOfHeroes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('testAngular');
+    expect(app.title).toEqual('TourOfHeroes');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to testAngular!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to TourOfHeroes!');
   });
 });
